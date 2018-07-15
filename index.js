7ecd0fc0733efe8d01ebec5f751fe3d34cf371fa
leMaik <an@maik.tk>
2018-07-15 20:11:37 +0200

    Add an interactive REPL.

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const Interpreter = require('./Interpreter')
 const lexer = new Lexer()
 const interpreter = new Interpreter()
 
-process.stdin.resume()
-process.stdin.on('data', (buf) => lexer.put(buf.toString()))
-process.stdin.on('end', () => {
-  for (const token of lexer.getTokens()) {
-    // console.log(token)
-    interpreter.execute(token)
+if (process.argv[process.argv.length - 1] === '--') {
+  process.stdin.resume()
+  process.stdin.on('data', (buf) => lexer.put(buf.toString()))
+  process.stdin.on('end', () => {
+    for (const token of lexer.getTokens()) {
+      interpreter.execute(token)
+    }
+    console.log(interpreter._stack._stack.map(obj => obj.toString()).join(', '))
+  })
+} else {
+  const read = require("read")
+  function repl () {
+    read({ prompt: '>' }, (err, input) => {
+      if (!err) {
+        lexer.put(input)
+        for (const token of lexer.getTokens()) {
+          interpreter.execute(token)
+        }
+        console.log(interpreter._stack._stack.map(obj => obj.toString()).join(', '))
+        repl()
+      } else {
+        console.log('')
+      }
+    })
   }
-  console.log(interpreter._stack._stack.map(obj => obj.toString()).join(', '))
-})
+  repl()
+}
