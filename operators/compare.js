2528a7750072031fdea82b5d43090d28009dcbaf
leMaik <an@maik.tk>
2018-07-20 22:24:22 +0200

    Implement equal, not equal, approximate equal and not approx equal operators.

--- operators/compare.js
+++ operators/compare.js
@@ -45,3 +45,87 @@ module.exports.greaterThanOrEqual = {
         interpreter._stack.push(new types.Bool(a >= b))
     }
 }
+
+function isEqual (a, b) {
+    if (a instanceof types.Arr && b instanceof types.Arr) {
+        if (a === b) {
+            return true
+        }
+        if (a.items.length === b.items.length) {
+            for (let i = 0; i < a.items.length; i++) {
+                if (!isEqual(a.items[i], b.items[i])) {
+                    return false
+                }
+            }
+            return true
+        }
+    } else if (a instanceof types.Num && b instanceof types.Num) {
+        return a.value === b.value
+    } else if (a instanceof types.Str && b instanceof types.Str) {
+        return a.value === b.value
+    } else if (a instanceof types.Bool && b instanceof types.Bool) {
+        return a.value === b.value
+    }
+    return false
+}
+
+function isApproxEqual (a, b, tolerance) {
+    if (a instanceof types.Arr && b instanceof types.Arr) {
+        if (a === b) {
+            return true
+        }
+        if (a.items.length === b.items.length) {
+            for (let i = 0; i < a.items.length; i++) {
+                if (!isApproxEqual(a.items[i], b.items[i])) {
+                    return false
+                }
+            }
+            return true
+        }
+    } else if (a instanceof types.Num && b instanceof types.Num) {
+        return Math.abs(a.value - b.value) <= tolerance
+    } else if (a instanceof types.Str && b instanceof types.Str) {
+        return a.value === b.value
+    } else if (a instanceof types.Bool && b instanceof types.Bool) {
+        return a.value === b.value
+    }
+    return false
+}
+
+module.exports.equal = {
+    name: '=',
+    execute: (interpreter) => {
+        const a = interpreter._stack.pop()
+        const b = interpreter._stack.pop()
+        interpreter._stack.push(new types.Bool(isEqual(a, b)))
+    }
+}
+
+module.exports.notEqual = {
+    name: '!=',
+    execute: (interpreter) => {
+        const a = interpreter._stack.pop()
+        const b = interpreter._stack.pop()
+        interpreter._stack.push(new types.Bool(!isEqual(a, b)))
+    }
+}
+
+module.exports.approxEqual = {
+    name: '~=',
+    execute: (interpreter) => {
+        const tolerance = interpreter._stack.popNumber()
+        const a = interpreter._stack.pop()
+        const b = interpreter._stack.pop()
+        interpreter._stack.push(new types.Bool(isApproxEqual(a, b, tolerance.value)))
+    }
+}
+
+module.exports.notApproxEqual = {
+    name: '!~=',
+    execute: (interpreter) => {
+        const tolerance = interpreter._stack.popNumber()
+        const a = interpreter._stack.pop()
+        const b = interpreter._stack.pop()
+        interpreter._stack.push(new types.Bool(!isApproxEqual(a, b, tolerance.value)))
+    }
+}
