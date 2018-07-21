eb296b2271fbb108469a9ccb35435cb446c0cc15
leMaik <an@maik.tk>
2018-07-21 20:35:11 +0200

    Implement cond.

--- operators/controlflow.js
+++ operators/controlflow.js
@@ -18,3 +18,38 @@ module.exports.if = {
     }
   }
 }
+
+module.exports.cond = {
+  name: 'cond',
+  execute: (interpreter, token) => {
+    const pairs = interpreter._stack.pop()
+    if (!(pairs instanceof types.Arr)) {
+      throw new types.Err(`cond expects an array (:Arr) with conditions followed by actions but got ${pairs.getTypeName()}`, token)
+    }
+
+    for (let i = 0; i < pairs.items.length; i += 2) {
+      const cond = pairs.items[i]
+      if (!(cond instanceof types.ExeArr)) {
+        throw new types.Err(`cond expected a condition array (:ExeArr) but found ${cond.getTypeName()}`, cond.origin)
+      }
+      if (i >= pairs.length) {
+        throw new types.Err('cond expected the condition array to be followed by an action array (:ExeArr) but it is missing', cond.origin)
+      }
+      const action = pairs.items[i + 1]
+      if (!(action instanceof types.ExeArr)) {
+        throw new types.Err(`cond expected an action array (:ExeArr) but found ${action.getTypeName()}`, action.origin)
+      }
+
+      interpreter.executeObj(cond)
+      const result = interpreter._stack.pop()
+      if (result instanceof types.Bool) {
+        if (result.value) {
+          interpreter.executeObj(action)
+          break // break at first matching condition
+        }
+      } else {
+        throw new types.Err(`cond expected the condition array to result in a :Bool, but got ${result.getTypeName()} instead`, cond.origin)
+      }
+    }
+  }
+}
