c7685bb7338c16c0845ab749082df8ed166c5bea
leMaik <an@maik.tk>
2018-07-22 19:24:35 +0200

    Implement for and fori.

--- operators/controlflow.js
+++ operators/controlflow.js
@@ -73,6 +73,103 @@ module.exports.loop = {
   }
 }
 
+module.exports.for = {
+  name: 'for',
+  *execute (interpreter, token) {
+    const body = interpreter._stack.pop()
+    if (!(body instanceof types.ExeArr)) {
+      throw new types.Err(`for expects an :ExeArr but got ${body.getTypeName()}`, token)
+    }
+    if (interpreter._stack.peek() instanceof types.Int) {
+      const upper = interpreter._stack.pop()
+      const lower = interpreter._stack.pop()
+      if (lower instanceof types.Int) {
+        try {
+          let i
+          for (i = lower.value; i < upper.value; i++) {
+            interpreter._stack.push(new types.Int(i))
+            yield* interpreter.executeObj(body)
+          }
+        } catch (e) {
+          if (e !== 'break') {
+            throw e
+          }
+        }
+      } else {
+        throw new types.Err(`for expects lower bound to be an :Int but got ${lower.getTypeName()} instead`, token)
+      }
+    } else {
+      const arr = interpreter._stack.pop()
+      if (arr instanceof types.Arr) {
+        try {
+          for (const item of arr.items) {
+            interpreter._stack.push(item)
+            yield* interpreter.executeObj(body)
+          }
+        } catch (e) {
+          if (e !== 'break') {
+            throw e
+          }
+        }
+      } else if (arr instanceof types.Str) {
+        try {
+          for (const char of arr.value.split('')) {
+            interpreter._stack.push(new types.Str(char))
+            yield* interpreter.executeObj(body)
+          }
+        } catch (e) {
+          if (e !== 'break') {
+            throw e
+          }
+        }
+      } else {
+        throw new types.Err(`for can only iterate over :Str and :Arr but got ${arr.getTypeName()}`, token)
+      }
+    }
+  }
+}
+
+module.exports.fori = {
+  name: 'fori',
+  *execute (interpreter, token) {
+    const body = interpreter._stack.pop()
+    if (!(body instanceof types.ExeArr)) {
+      throw new types.Err(`fori expects an :ExeArr but got ${body.getTypeName()}`, token)
+    }
+    const arr = interpreter._stack.pop()
+    if (arr instanceof types.Arr) {
+      try {
+        let i
+        for (i = 0; i < arr.items.length; i++) {
+          interpreter._stack.push(arr.items[i])
+          interpreter._stack.push(new types.Int(i))
+          yield* interpreter.executeObj(body)
+        }
+      } catch (e) {
+        if (e !== 'break') {
+          throw e
+        }
+      }
+    } else if (arr instanceof types.Str) {
+      try {
+        const chars = arr.value.split('')
+        let i
+        for (i = 0; i < chars.length; i++) {
+          interpreter._stack.push(new types.Str(chars[i]))
+          interpreter._stack.push(new types.Int(i))
+          yield* interpreter.executeObj(body)
+        }
+      } catch (e) {
+        if (e !== 'break') {
+          throw e
+        }
+      }
+    } else {
+      throw new types.Err(`fori can only iterate over :Str and :Arr but got ${arr.getTypeName()}`, token)
+    }
+  }
+}
+
 module.exports.break = {
   name: 'break',
   execute: () => {
