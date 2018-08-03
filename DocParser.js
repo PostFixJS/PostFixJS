21aeda5ff8b3736ddfe1ce665780601581ab63cc
leMaik <an@maik.tk>
2018-08-03 20:01:20 +0200

    Implement parsing of doc comments and add many tests for that.

--- DocParser.js
+++ DocParser.js
@@ -1,82 +1,135 @@
-const fs = require('fs')
 const Lexer = require('./Lexer')
 
-const tokens = Lexer.parse(fs.readFileSync('./test.pf', 'utf-8'), { emitComments: true })
-for (let i = 0; i < tokens.length; i++) {
-    const fn = getFunctionAt(tokens, i)
-    if (fn) {
-        console.log(fn)
+class DocParser {
+  static * getFunctions (code) {
+    const tokens = Lexer.parse(code, { emitComments: true })
+    for (let i = 0; i < tokens.length; i++) {
+      const fn = getFunctionAt(tokens, i)
+      if (fn !== false) {
+        yield fn.fn
+        i = fn.i
+      }
     }
+  }
 }
 
 function getFunctionAt (tokens, i) {
-    const fn = {}
-    if (tokens[i].tokenType === 'BLOCK_COMMENT') {
-        fn.doc = tokens[i].token
-        i++
-        if (tokens[i].tokenType === 'SYMBOL') {
-            fn.name = tokens[i].token
-        }
-        i++
-        const params = readParamsList(tokens, i)
-        if (params) {
-            i = params.i
-            const paramsAndReturns = parseParamsList(params.params)
-            fn.params = paramsAndReturns.params
-            fn.returns = paramsAndReturns.returns
-        }
-        console.log(fn)
-        skipExeArr(tokens, i)
-        if (tokens[i].tokenType === 'SYMBOL' && tokens[i].token === 'fun') {
-            return fn
-        }
-    }
-    return false
+  const fn = {}
+  let doc
+  if (tokens[i].tokenType === 'BLOCK_COMMENT') {
+    doc = parseDocComment(tokens[i].token)
+    fn.description = doc.description
+    i++
+  } else {
+    doc = { params: {}, returns: [] }
+    fn.description = undefined
+  }
+
+  if (tokens[i].tokenType === 'SYMBOL') {
+    const token = tokens[i].token
+    fn.name = token.indexOf(':') === 0
+      ? token.substr(1)
+      : token.substr(0, token.length - 1)
+  }
+  i++
+  const params = readParamsList(tokens, i)
+  if (params) {
+    i = params.i
+    const paramsAndReturns = parseParamsList(params.params)
+    fn.params = paramsAndReturns.params.map((param) => ({
+      ...param,
+      description: doc.params[param.name]
+    }))
+    fn.returns = paramsAndReturns.returns.map((type, i) => ({
+      type,
+      description: doc.returns.length > i ? doc.returns[i].description : undefined
+    }))
+  }
+  i = skipExeArr(tokens, i)
+  if (tokens[i] && tokens[i].tokenType === 'REFERENCE' && tokens[i].token === 'fun') {
+    return { fn, i }
+  }
+  return false
 }
 
-function readParamsList(tokens, i) {
-    if (tokens[i].tokenType === 'PARAM_LIST_START') {
-        const params = []
-        while (i < tokens.length && tokens[i - 1].tokenType !== 'PARAM_LIST_END') {
-            params.push(tokens[i])
-            i++
-        }
-        return {params,i}
+function readParamsList (tokens, i) {
+  if (tokens[i] && tokens[i].tokenType === 'PARAM_LIST_START') {
+    const params = []
+    while (i < tokens.length && tokens[i - 1].tokenType !== 'PARAM_LIST_END') {
+      params.push(tokens[i])
+      i++
     }
-    return false
+    return {params, i}
+  }
+  return false
 }
 
 function skipExeArr (tokens, i) {
-    let arrs = 1
-    i++
-    for (; i < tokens.length; i++) {
-        if (tokens[i].tokenType === 'EXEARR_START') {
-            arrs++
-        } else if (tokens[i].tokenType === 'EXEARR_END') {
-            arrs--
-        }
-        if (arrs === 0) {
-            return i + 1
-        }
+  let arrs = 1
+  i++
+  for (; i < tokens.length; i++) {
+    if (tokens[i].tokenType === 'EXEARR_START') {
+      arrs++
+    } else if (tokens[i].tokenType === 'EXEARR_END') {
+      arrs--
     }
-    return false
+    if (arrs === 0) {
+      return i + 1
+    }
+  }
+  return false
 }
 
 function parseParamsList (paramsList) {
-    const arrowRightPosition = paramsList.findIndex((token) => token.tokenType === 'RIGHT_ARROW')
+  const arrowRightPosition = paramsList.findIndex((token) => token.tokenType === 'RIGHT_ARROW')
 
-    const params = []
-    for (let i = 1; i < arrowRightPosition; i++) {
-        const value = paramsList[i].token
-        if (value[0] === ':' || value[value.length - 1] === ':') {
-            // this is a symbol
-            params[params.length - 1].type = value
-        } else {
-            // this is a parameter name
-            params.push({ name: value })
-        }
+  const params = []
+  for (let i = 1; i < arrowRightPosition; i++) {
+    const value = paramsList[i].token
+    if (value[0] === ':' || value[value.length - 1] === ':') {
+      // this is a symbol
+      params[params.length - 1].type = value
+    } else {
+      // this is a parameter name
+      params.push({ name: value, type: undefined })
     }
+  }
 
-    const returns = paramsList.slice(arrowRightPosition + 1, -1).map((token) => token.token)
-    return { params, returns }
+  const returns = arrowRightPosition >= 0 ? paramsList.slice(arrowRightPosition + 1, -1).map((token) => token.token) : []
+  return { params, returns }
 }
+
+function parseDocComment (comment) {
+  const lines = comment.substring(2, comment.length - 2).trim().split('\n')
+  const firstParam = lines.findIndex((line) => line.trim()[0] === '@')
+  const tags = (firstParam >= 0 ? lines.slice(firstParam) : [])
+    .filter((line) => line[0] === '@')
+    .map((line) => {
+      const match = line.match(/^(@.+?)\s+(.+?)$/)
+      return { tag: match[1], value: match[2] }
+    })
+
+  const params = tags
+    .filter(({ tag }) => tag === '@param')
+    .map(({ value }) => {
+      const match = value.match(/(.+?)\s+(.+?)$/)
+      return match ? { name: match[1], description: match[2] } : null
+    })
+    .filter((param) => param != null)
+    .reduce((params, { name, description }) => {
+      params[name] = description
+      return params
+    }, {})
+
+  const returns = tags
+    .filter(({ tag }) => tag === '@return')
+    .map(({ value }) => ({ description: value }))
+
+  return {
+    description: (firstParam >= 0 ? lines.slice(0, firstParam) : lines).join('\n') || undefined,
+    params,
+    returns
+  }
+}
+
+module.exports = DocParser
---
+++ DocParser.test.js
@@ -0,0 +1,170 @@
+import test from 'ava'
+import DocParser from './DocParser'
+
+function getFunctions (code) {
+  return Array.from(DocParser.getFunctions(code))
+}
+
+test('DocParser gets the signature of a function', (t) => {
+  const functions = getFunctions(`
+#<
+Calculate the factorial of the given number.
+@param n Number
+@return Factorial of n
+>#
+fac: (n :Int -> :Int) {
+  { n n 1 - recur * } { 1 } n 1 > if
+} fun
+  `)
+
+  t.deepEqual(functions, [{
+    name: 'fac',
+    description: 'Calculate the factorial of the given number.',
+    params: [
+      { name: 'n', type: ':Int', description: 'Number' }
+    ],
+    returns: [
+      { type: ':Int', description: 'Factorial of n' }
+    ]
+  }])
+})
+
+test('DocParser gets the signature of an undocumented function', (t) => {
+  const functions = getFunctions(`
+fac: (n :Int -> :Int) {
+  { n n 1 - recur * } { 1 } n 1 > if
+} fun
+  `)
+
+  t.deepEqual(functions, [{
+    name: 'fac',
+    description: undefined,
+    params: [
+      { name: 'n', type: ':Int', description: undefined }
+    ],
+    returns: [
+      { type: ':Int', description: undefined }
+    ]
+  }])
+})
+
+test('DocParser supports missing function descriptions', (t) => {
+  const functions = getFunctions(`
+#<
+@param n Some number
+>#
+foo: (n :Int -> :Str) {} fun
+  `)
+
+  t.deepEqual(functions, [{
+    name: 'foo',
+    description: undefined,
+    params: [
+      { name: 'n', type: ':Int', description: 'Some number' }
+    ],
+    returns: [
+      { type: ':Str', description: undefined }
+    ]
+  }])
+})
+
+test('DocParser supports missing @param tags', (t) => {
+  const functions = getFunctions(`
+#<
+A function that does things.
+@return The resulting string
+>#
+foo: (n :Int -> :Str) {} fun
+  `)
+
+  t.deepEqual(functions, [{
+    name: 'foo',
+    description: 'A function that does things.',
+    params: [
+      { name: 'n', type: ':Int', description: undefined }
+    ],
+    returns: [
+      { type: ':Str', description: 'The resulting string' }
+    ]
+  }])
+})
+
+test('DocParser supports incomplete @param tags', (t) => {
+  const functions = getFunctions(`
+#<
+A function that does things.
+@param a
+@param b
+@return The resulting string
+>#
+foo: (a b -> :Str) {} fun
+  `)
+
+  t.deepEqual(functions, [{
+    name: 'foo',
+    description: 'A function that does things.',
+    params: [
+      { name: 'a', type: undefined, description: undefined },
+      { name: 'b', type: undefined, description: undefined },
+    ],
+    returns: [
+      { type: ':Str', description: 'The resulting string' }
+    ]
+  }])
+})
+
+test('DocParser supports missing @return tags', (t) => {
+  const functions = getFunctions(`
+#<
+A function that does things.
+>#
+foo: (-> :Str) {} fun
+  `)
+
+  t.deepEqual(functions, [{
+    name: 'foo',
+    description: 'A function that does things.',
+    params: [],
+    returns: [
+      { type: ':Str', description: undefined }
+    ]
+  }])
+})
+
+test('DocParser supports functions that return nothing', (t) => {
+  t.deepEqual(getFunctions('noop: (->) {} fun'), [{
+    name: 'noop',
+    description: undefined,
+    params: [],
+    returns: []
+  }])
+
+  t.deepEqual(getFunctions('noop: () {} fun'), [{
+    name: 'noop',
+    description: undefined,
+    params: [],
+    returns: []
+  }])
+})
+
+test('DocParser supports functions with multiple return values', (t) => {
+  t.deepEqual(getFunctions(`
+#<
+Get the first two elements of an array.
+@param arr Array
+@return First element
+@return Second element
+>#
+firstTwo: (arr :Arr -> :Obj :Obj) {} fun
+`), [{
+    name: 'firstTwo',
+    description: 'Get the first two elements of an array.',
+    params: [
+      { name: 'arr', type: ':Arr', description: 'Array' }
+    ],
+    returns: [
+      { type: ':Obj', description: 'First element' },
+      { type: ':Obj', description: 'Second element' }
+    ]
+  }])
+})
