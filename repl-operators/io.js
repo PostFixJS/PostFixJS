15df1e2cb2afea795f853f3e5ea6e63a425fad4f
leMaik <an@maik.tk>
2018-08-18 22:55:17 +0200

    Implement printf and printfln for the repl.

--- repl-operators/io.js
+++ repl-operators/io.js
@@ -1,3 +1,6 @@
+const { format } = require('../operators/impl/format')
+const types = require('../types')
+
 module.exports.print = {
   name: 'print',
   execute (interpreter) {
@@ -9,5 +12,29 @@ module.exports.println = {
   name: 'println',
   execute (interpreter) {
     process.stdout.write(`${interpreter._stack.pop().value}\n`)
+  }
+}
+
+module.exports.printf = {
+  name: 'printf',
+  execute (interpreter, token) {
+    const params = interpreter._stack.pop()
+    if (!(params instanceof types.Arr)) {
+      throw new types.Err(`printf expects an :Arr with parameters as second argument but got ${params.getTypeName()} instead`, token)
+    }
+    const formatStr = interpreter._stack.popString().value
+    process.stdout.write(format(formatStr, params))
+  }
+}
+
+module.exports.printfln = {
+  name: 'printfln',
+  execute (interpreter, token) {
+    const params = interpreter._stack.pop()
+    if (!(params instanceof types.Arr)) {
+      throw new types.Err(`printfln expects an :Arr with parameters as second argument but got ${params.getTypeName()} instead`, token)
+    }
+    const formatStr = interpreter._stack.popString().value
+    process.stdout.write(`${format(formatStr, params)}\n`)
   }
 }
