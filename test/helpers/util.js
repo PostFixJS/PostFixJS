23ee74e5a22dd9af482876e0b0d9cef6d230abb5
leMaik <an@maik.tk>
2018-08-23 00:06:17 +0200

    Add a test utility to execute code with a timeout.

--- test/helpers/util.js
+++ test/helpers/util.js
@@ -16,6 +16,43 @@ function execute (code) {
 }
 
 /**
+ * Execute the given code and return the resulting stack and dictionary stack.
+ * This is useful to test things that may result in an infinite loop.
+ * @param {string} code Code to execute
+ * @param {number} timeout Execution timeout, in milliseconds
+ * @returns {object} Promise of an object that contains the stack and dictStack
+ */
+function executeTimeout (code, timeout = 1000) {
+  let cancel = false
+  const timeoutTimer = setTimeout(() => {
+    cancel = true
+  }, timeout)
+  const done = new Promise((resolve, reject) => {
+    setImmediate(() => {
+      const interpreter = new Interpreter()
+      interpreter.startRun(Lexer.parse(code))
+      const continueExecution = () => {
+        const { done } = interpreter.step()
+        if (done) {
+          clearTimeout(timeoutTimer)
+          resolve(interpreter)
+        } else if (cancel) {
+          clearTimeout(timeoutTimer)
+          reject(new Error(`Execution timed out after ${timeout} ms`))
+        } else {
+          setImmediate(continueExecution)
+        }
+      }
+      continueExecution()
+    })
+  })
+  return done.then((interpreter) => ({
+    stack: interpreter._stack,
+    dictStack: interpreter._dictStack
+  }))
+}
+
+/**
  * Create a function that checks if the message of a given error matches a string.
  * @param {string} expectedMessage Expected error message
  * @returns {function} Function that checks if the message of the error object matches the expected message
@@ -28,5 +65,6 @@ function checkErrorMessage (expectedMessage) {
 
 module.exports = {
   checkErrorMessage,
-  execute
+  execute,
+  executeTimeout
 }
