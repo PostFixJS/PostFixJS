603ce2c347139bd37d1082215534add4f9c7588f
leMaik <an@maik.tk>
2018-09-20 01:04:16 +0200

    Make array accessor functions reusable.

--- operators/array.js
+++ operators/array.js
@@ -2,38 +2,7 @@ const types = require('../types')
 const { nextInt } = require('./impl/random')
 const { isEqual, compare } = require('./impl/compare')
 const { popOperand, popOperands, checkOperands } = require('../typeCheck')
-
-function keyGet (array, key, defaultValue) {
-  for (let i = 0; i < array.items.length - 1; i++) {
-    if (isEqual(key, array.items[i])) {
-      return array.items[i + 1]
-    }
-  }
-  return defaultValue
-}
-
-function keySet (array, key, value) {
-  for (let i = 0; i < array.items.length - 1; i++) {
-    if (isEqual(key, array.items[i])) {
-      const newArr = array.copy()
-      newArr.items[i + 1] = value
-      return newArr
-    }
-  }
-  const newArr = array.copy()
-  newArr.items.push(key, value)
-  return newArr
-}
-
-function arraySet (array, index, value, token) {
-  if (index.value >= 0 && index.value < array.items.length) {
-    const newArr = array.copy()
-    newArr.items[index.value] = value
-    return newArr
-  } else {
-    throw new types.Err(`Index is out of range (index is ${index.value} but the :Arr has length ${array.items.length})`, token)
-  }
-}
+const { keyGet, keySet, arraySet } = require('./impl/array')
 
 module.exports.length = {
   name: 'length',
---
+++ operators/impl/array.js
@@ -0,0 +1,40 @@
+const types = require('../../types')
+const { isEqual } = require('./compare')
+
+function keyGet (array, key, defaultValue) {
+  for (let i = 0; i < array.items.length - 1; i++) {
+    if (isEqual(key, array.items[i])) {
+      return array.items[i + 1]
+    }
+  }
+  return defaultValue
+}
+
+function keySet (array, key, value) {
+  for (let i = 0; i < array.items.length - 1; i++) {
+    if (isEqual(key, array.items[i])) {
+      const newArr = array.copy()
+      newArr.items[i + 1] = value
+      return newArr
+    }
+  }
+  const newArr = array.copy()
+  newArr.items.push(key, value)
+  return newArr
+}
+
+function arraySet (array, index, value, token) {
+  if (index.value >= 0 && index.value < array.items.length) {
+    const newArr = array.copy()
+    newArr.items[index.value] = value
+    return newArr
+  } else {
+    throw new types.Err(`Index is out of range (index is ${index.value} but the :Arr has length ${array.items.length})`, token)
+  }
+}
+
+module.exports = {
+  keyGet,
+  keySet,
+  arraySet
+}
