bfafabc1e8007deef33ca446868f41ede07ff7f3
Maik Marschner <an@maik.tk>
2018-09-25 23:59:44 +0200

    Give time tests more time.

--- operators/time.test.js
+++ operators/time.test.js
@@ -7,11 +7,11 @@ test('time gets the current time in milliseconds', async (t) => {
   t.is(stack.count, 1)
   const time = stack.pop()
   t.true(time instanceof types.Int)
-  t.true(Math.abs(Date.now() - time.value) < 10)
+  t.true(Math.abs(Date.now() - time.value) < 50)
 })
 
 test('sleep should sleep for the given delay', async (t) => {
   const start = Date.now()
   await execute('100 sleep')
-  t.true(Math.abs(100 - (Date.now() - start)) < 10)
+  t.true(Math.abs(100 - (Date.now() - start)) < 50)
 })
