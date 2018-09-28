44847ee4bb4be1fd12630bb9b7346b47376881ad
Maik Marschner <an@maik.tk>
2018-09-28 20:47:01 +0200

    Only allow addidition and string and array concatenation with the + operator. Add tests for it.

--- operators/math.js
+++ operators/math.js
@@ -7,21 +7,22 @@ module.exports.plus = {
     const a = interpreter._stack.pop()
     const b = interpreter._stack.pop()
     checkOperands([
-      { value: a, type: ['Int', 'Flt', 'Str', 'Arr'] },
-      { value: b, type: ['Int', 'Flt', 'Str', 'Arr'] }
+      { value: a, type: ['Num', 'Str', 'Arr'] },
+      { value: b, type: ['Num', 'Str', 'Arr'] }
     ], token)
 
-    if (a instanceof types.Str || b instanceof types.Str) {
+    if (a instanceof types.Str && b instanceof types.Str) {
       interpreter._stack.push(new types.Str(b.value + a.value))
     } else if (a instanceof types.Arr && b instanceof types.Arr) {
       interpreter._stack.push(new types.Arr([...b.items, ...a.items]))
-    } else if (a instanceof types.Flt || b instanceof types.Flt) {
-      interpreter._stack.push(new types.Flt(b.value + a.value))
-    } else if (a instanceof types.Int && b instanceof types.Int) {
-      interpreter._stack.push(new types.Int(b.value + a.value))
+    } else if (a instanceof types.Num && b instanceof types.Num) {
+      if (a instanceof types.Flt || b instanceof types.Flt) {
+        interpreter._stack.push(new types.Flt(b.value + a.value))
+      } else {
+        interpreter._stack.push(new types.Int(b.value + a.value))
+      }
     } else {
-      // trying to concatenate an :Arr with anything other than :Arr
-      throw new types.Err(`+ can not concatenate ${a.getTypeName()} and ${b.getTypeName()}`, token)
+      throw new types.Err(`+ can only add numbers and concatenate strings or arrays, but got ${b.getTypeName()} and ${a.getTypeName()}`, token)
     }
   }
 }
--- operators/math.test.js
+++ operators/math.test.js
@@ -1,6 +1,36 @@
 import test from 'ava'
-const { execute } = require('../test/helpers/util')
+const { execute, throwsErrorMessage, checkErrorMessage } = require('../test/helpers/util')
 const types = require('../types')
+
+test('+ should add two numbers and return :Int if both are :Ints', async (t) => {
+  const { stack } = await execute('1 2 + 1.5 2 + 5 5.2 +')
+  t.is(stack.count, 3)
+  const sum3 = stack.pop()
+  t.true(sum3 instanceof types.Flt)
+  t.is(sum3.value, 10.2)
+  const sum2 = stack.pop()
+  t.true(sum2 instanceof types.Flt)
+  t.is(sum2.value, 3.5)
+  const sum1 = stack.pop()
+  t.true(sum1 instanceof types.Int)
+  t.is(sum1.value, 3)
+})
+
+test('+ should concatenate two strings', async (t) => {
+  const { stack } = await execute('"hello" " " "world" + +')
+  t.is(stack.count, 1)
+  t.is(stack.pop().value, 'hello world')
+})
+
+test('+ should concatenate two arrays', async (t) => {
+  const { stack } = await execute('[1 2] [3 4] +')
+  t.is(stack.count, 1)
+  t.deepEqual(stack.pop().items.map((i) => i.value), [1, 2, 3, 4])
+})
+
+test('+ should throw if the operands cannot be concatenated', async (t) => {
+  await throwsErrorMessage(t, () => execute('"1+1=" 2 +'), checkErrorMessage('+ can only add numbers and concatenate strings or arrays, but got :Str and :Int'))
+})
 
 test('* should return an Int if both operands are Ints', async (t) => {
   const { stack } = await execute('4 8 *')
