18dc550205cabd179cca11c4d8c42b45fa173ad2
leMaik <an@maik.tk>
2018-10-26 23:32:18 +0200

    Document the cancellation token.

--- util/cancellationToken.js
+++ util/cancellationToken.js
@@ -1,14 +1,35 @@
+/**
+ * Create a cancellation token and a cancel function.
+ *
+ * This is somewhat inspired by monaco's CancellationTokenSource and
+ * TC39's Cancellation API proposal.
+ */
 function createCancellationToken () {
   const onCancelListeners = []
   const token = {
+    /**
+     * Whether or not the action is cancelled.
+     */
     cancelled: false,
+
+    /**
+     * Add a callback function to invoke on cancellation.
+     * @param {function} listener Function to invoke on cancellation
+     */
     onCancel (listener) {
       onCancelListeners.push(listener)
     }
   }
 
   return {
+    /**
+     * The cancellation token that is used to check if the action was cancelled.
+     */
     token,
+
+    /**
+     * Cancel the action.
+     */
     cancel () {
       if (!token.cancelled) {
         token.cancelled = true
